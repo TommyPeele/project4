4d6b6ceea58e45f96605c5a05979da2e28db25f8
TommyPeele <tommypeele@gmail.com>
2015-04-16 18:06:36 -0400

    Added header comment

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,17 @@
+//
+// Project 4
+// Name: <your name>
+// E-mail: <e-mail address>
+// Instructor: Singh
+// COSC 150
+//
+// In accordance with the class policies and Georgetown's Honor Code,
+// I certify that, with the exceptions of the lecture notes and those
+// items noted below, I have neither given nor received any assistance
+// on this project.
+//
+// Description: <Describe your program>
+//
 
 public class Game {
 
