a935303f05b5f1aac09349835764c1503ddb1327
TommyPeele <tommypeele@gmail.com>
2015-04-17 23:30:14 -0400

    Updated header, removed TODO

--- src/Game.java
+++ src/Game.java
@@ -1,23 +1,22 @@
 //
 // Project 4
-// Name: <your name>
-// E-mail: <e-mail address>
+// Names: Ching-Hao Hu, Tommy Peele, Patrick Stocklin, Xavier Weisenreder
+// E-mails: ch931@georgetown.edu, btp11@georgetown.edu, pcs38@georgetown.edu, xmw2@georgetown.edu
 // Instructor: Singh
 // COSC 150
 //
 // In accordance with the class policies and Georgetown's Honor Code,
-// I certify that, with the exceptions of the lecture notes and those
-// items noted below, I have neither given nor received any assistance
+// we certify that, with the exceptions of the lecture notes and those
+// items noted below, we have neither given nor received any assistance
 // on this project.
 //
-// Description: <Describe your program>
+// Description: A game designed to teach teenagers about the dangers of texting and driving.
 //
 
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		return;
 	}
 
 }
