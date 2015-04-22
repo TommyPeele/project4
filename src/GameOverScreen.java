139701846bf3b1554ffdd4430e481c62ce415e01
Xaviermw <xmw2@georgetown.edu>
2015-04-22 19:42:21 -0400

    Made GameOverScreen an Implementation of the Main Screen with Score added

--- src/GameOverScreen.java
+++ src/GameOverScreen.java
@@ -1,10 +1,22 @@
-
-public class GameOverScreen extends Screen{
-	
+public class GameOverScreen extends StartScreen
+{
 	private static final long serialVersionUID = 0;
+	private static JLabel scoreBar;
+	private static String scoreInfo;
 	
-	public GameOverScreen(Game game){
+	public GameOverScreen(Game game)
+	{
 		super(game);
+		gameOverValues();
+	
 	}
 
+	public void gameOverValues()
+	{
+		scoreBar.setBounds(40, 100, 520, 20);
+		introText.setLabel("You Life Isn't A Game, Don't Text And Drive");
+		scoreInfo = "Final Score: " + String.valueOf(game.getScore());
+		scoreBar.setLabel(scoreInfo);	
+	}
+	
 }
