274a551c903a7692665efa26ef917b7c2284c57c
Ching-Hao Hu (Camden) <huchinghao@hotmail.com>
2015-04-26 21:43:04 -0400

    added instructions to screen

--- src/InstructionScreen.java
+++ src/InstructionScreen.java
@@ -19,20 +19,34 @@ public class InstructionScreen extends Screen{
 	}
 	
 	public void initGUI(){
-		JButton webButton = new JButton("Learn More Statistics Here");
-		JButton multiButton = new JButton("Learn More About The Dangers of Multitasking Here");
-		
-		JLabel webQuote = new JLabel("13% of Drivers aged 18-20 involved in car wrecks admitted to talking or texting on the time of their crash.");
-		JLabel multiQuote = new JLabel("Multitasking doesn’t just slow you down and increase the number of mistakes you make; it temporarily changes the way your brain works.");
-		
+		JLabel instruction = new JLabel();
 		JButton startButton = new JButton("Return to Main Screen");
 		
+		instruction.setText("<html>Use the left and right arrows to move the car between lanes.<br>"
+		+ "A red circle in the upper left corner will flash when you have a new text message!<br>"
+		+ "Use the down arrow to pull down the text screen<br>" 
+		+ "Use the up arrow to pull up the text screen<br>"
+		+ "Type the text and press Enter to send the text<br>"
+		+ "Avoid texting with a police car on the road<br>"
+		+ "Avoid accidents with other cars</html>");
+		
+		instruction.setHorizontalAlignment(SwingConstants.CENTER);
 		setLayout(null);
 		
+		instruction.setBounds(50,150,500,150);
 		startButton.setBounds(200,320,200,50);
 
+		add(instruction);
 		add(startButton);
 	}
 	
-    
+	class StartButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+        	game.getContentPane().removeAll();
+        	game.currentScreen = new StartScreen(game);
+        	game.getContentPane().add(game.currentScreen);
+        	game.getContentPane().validate();
+        }
+    }
 }
