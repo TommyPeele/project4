21938501cae054a730e7a41487f05f7e45088f24
Ching-Hao Hu (Camden) <huchinghao@hotmail.com>
2015-04-26 20:18:22 -0400

    instruction added

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -14,20 +14,30 @@ public class StartScreen extends Screen{
 	}
 	
 	public void initGUI(){
+		JLabel instruction = new JLabel();
 		JButton startButton = new JButton("Start Game");
 		JButton infoButton = new JButton("Learn More");
 		JButton exitButton = new JButton("Exit");
 		
 		setLayout(null);
 		
+		instruction.setBounds(100,150,300,50);
 		startButton.setBounds(200,200,200,50);
 		infoButton.setBounds(200,270,200,50);
 		exitButton.setBounds(200,340,200,50);
+		
+		instruction.setText("Use the left and right arrows to move the car between lanes.\n"
+		+ "Use the down arrow to pull down the text screen\n" 
+		+ "Use the up arrow to pull up the text screen\n"
+		+ "Type the text and press Enter to send the text\n"
+		+ "Avoid texting with a police car on the road\n"
+		+ "Avoid accidents with other cars")
 		
 		startButton.addActionListener(new StartButtonListener());
 		infoButton.addActionListener(new InfoButtonListener());
 		exitButton.addActionListener(new ExitButtonListener());
 		
+		add(instruction);
 		add(startButton);
 		add(infoButton);
 		add(exitButton);
