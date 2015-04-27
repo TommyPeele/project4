0580d46abac84318ae08c704cfd9b9ef11d5069c
Ching-Hao Hu (Camden) <huchinghao@hotmail.com>
2015-04-26 21:00:20 -0400

    Update BackgroundScreen.java

--- src/BackgroundScreen.java
+++ src/BackgroundScreen.java
@@ -1,4 +1,8 @@
 
 public class BackgroundScreen {
-
+	Image img = new ImageIcon("car-accident.jpg").getImage();
+    @Override
+    public void paintComponent(Graphics g) {
+        g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+    }
 }
