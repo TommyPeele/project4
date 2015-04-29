cf83e7861478923e659c7b87f7ccdc9c7bacb7c8
Xaviermw <xmw2@georgetown.edu>
2015-04-28 20:21:47 -0700

    Makes Sure Obstacle Cars Spawn in the Right Lane

---
+++ src/tests/ObstacleCarLaneTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ObstacleCarLaneTest {
+
+	code.cars.ObstacleCar obsCar = new code.cars.ObstacleCar(0, 0);
+	
+	public void testChooseLane() {
+		int xValue = obsCar.getX();
+		if ((xValue != 185 && xValue != 285) && xValue != 385)
+			fail("Invalid X Value");
+	}
+
+}
